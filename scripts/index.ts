fb85a5bab4df4b4787e301d42ed8192d68cfda7e
hoshi <hoshi03918@gmail.com>
2021-03-05 17:39:21 +0900

    ブロックチェーンに本文より生成したハッシュチェーンを書き込むAPIを実装(未検証)

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,3 +1,4 @@
+import {} from '../scripts/types'
 // EXPRESS関連
 import express from 'express';
 import bodyParser from 'body-parser';
@@ -28,14 +29,14 @@ const securityLevel = 2;
 const seed ='PUEOTSEITFEVEWCWBTSIZM9NKRGJEIMXTULBACGFRQK9IMGICLBKW9TTEVSDQMGWKBXPVCBMMCXWMNPDX';
 var address;
 getAddress(seed, 1).then(function(ret_address){
-    address = ret_address;
+  address = ret_address;
 });
 const keypair = createKeyPair();
 
 /* 2. listen()メソッドを実行して4001番ポートで待ち受け。*/
 const serverPortNumber = 4001
 const server = app.listen(serverPortNumber, () => {
-    console.log("Node.js is listening to PORT:" + serverPortNumber);
+  console.log("Node.js is listening to PORT:" + serverPortNumber);
 });
 
 
@@ -66,24 +67,24 @@ function preparTransferMessage(address: string, data): Array<{
 type payload<data> =  {node: typeof iota; address: string, data: data;}
 
 function writeToTangle<payloadData>(payload: payload<payloadData>, callBack?: (bundleHash: string, addressHash: string) => void) {
-    const targetNode = payload.node;
-    const address = payload.address;
+  const targetNode = payload.node;
+  const address = payload.address;
 
-    const transfers = preparTransferMessage(address, payload.data);
-    targetNode.prepareTransfers(seed, transfers)
-        .then(trytes => {
-            return targetNode.sendTrytes(trytes, depth, minimumWeightMagnitude);
-        })
-        .then(bundle => {
-            const bundle_hash = bundle[0].hash;// このハッシュ値をデータベースに書き込む
-            console.log(bundle_hash + " <- transaction hash- ");
-            if (callBack) {
-                callBack(bundle[0].hash as string, bundle[0].address as string)
-            }
-        })
-        .catch(err => {
-            console.log(err);
-        })
+  const transfers = preparTransferMessage(address, payload.data);
+  targetNode.prepareTransfers(seed, transfers)
+      .then(trytes => {
+          return targetNode.sendTrytes(trytes, depth, minimumWeightMagnitude);
+      })
+      .then(bundle => {
+          const bundle_hash = bundle[0].hash;// このハッシュ値をデータベースに書き込む
+          console.log(bundle_hash + " <- transaction hash- ");
+          if (callBack) {
+              callBack(bundle[0].hash as string, bundle[0].address as string)
+          }
+      })
+      .catch(err => {
+          console.log(err);
+      })
 }
 
 // test API
@@ -92,45 +93,72 @@ app.get("/api/test", (req, res, next) => {
     writeToTangle<{msg: string}>({node: iota, address:address, data: {msg: 'test'}},)
 });
 
+app.post("/api/post", [
+  check("user_id").isInt(),
+  check("previous_hash").isString(),
+  check("hash").isString(),
+  check("index").isInt(),
+], (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  if (req.body) {
+    const data = {
+      user_id: req.body.user_id as number,
+      previous_hash: req.body.previous_hash as string,
+      hash: req.body.hash as string,
+      index: req.body.index as number
+    }
+    console.log(data)
+    writeToTangle<{
+      user_id: number,
+      previous_hash: string,
+      hash: string,
+      index: number
+    }>({node: iota, address:address, data: data})
+  }
+})
+
 async function getAddress(seed: string, index: number) {
     let newAddress = await iota.getNewAddress(seed, { index: index, security: securityLevel, total: 1 });
     return newAddress[0];
 }
 
 function createKeyPair() {
-    const max = Buffer.from(
-        "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364140",
-        "hex"
-    );
-    let isInvalid = true;
-    let privateKey;
+  const max = Buffer.from(
+      "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364140",
+      "hex"
+  );
+  let isInvalid = true;
+  let privateKey;
 
-    while (isInvalid) {
-        let privateKeySeed = secureRandom.randomBuffer(32);
-        if (Buffer.compare(max, privateKeySeed) === 1) {
-            isInvalid = false;
-        }
-        privateKey = privateKeySeed.toString("hex");
-    }
+  while (isInvalid) {
+      let privateKeySeed = secureRandom.randomBuffer(32);
+      if (Buffer.compare(max, privateKeySeed) === 1) {
+          isInvalid = false;
+      }
+      privateKey = privateKeySeed.toString("hex");
+  }
 
-    const keys = ecdsa.keyFromPrivate(privateKey);
-    const publicKey = keys.getPublic("hex");
+  const keys = ecdsa.keyFromPrivate(privateKey);
+  const publicKey = keys.getPublic("hex");
 
-    return ({"privateKey":privateKey, "publicKey":publicKey});
+  return ({"privateKey":privateKey, "publicKey":publicKey});
 }
 
 function createSignature(message, privateKey) {
-    const privateKeyPair = ecdsa.keyFromPrivate(privateKey);
-    const signature = ecdsa.sign(message, privateKeyPair).toDER('hex'); //ハッシュされたメッセージに対して秘密鍵で電子署名する。
-    return signature;
+  const privateKeyPair = ecdsa.keyFromPrivate(privateKey);
+  const signature = ecdsa.sign(message, privateKeyPair).toDER('hex'); //ハッシュされたメッセージに対して秘密鍵で電子署名する。
+  return signature;
 }
 
 function verifySignature(message, publicKey, signature) {
-    const publicKeyPair = ecdsa.keyFromPublic(publicKey, 'hex'); // 公開鍵で電子署名の真正を確認。
-    const isVerified = publicKeyPair.verify(message, signature);
-    return isVerified;
+  const publicKeyPair = ecdsa.keyFromPublic(publicKey, 'hex'); // 公開鍵で電子署名の真正を確認。
+  const isVerified = publicKeyPair.verify(message, signature);
+  return isVerified;
 }
 
 function stackHash(previous_hash, current_hash) {
-    return crypto.createHash('sha256').update(previous_hash + current_hash).digest('hex');
+  return crypto.createHash('sha256').update(previous_hash + current_hash).digest('hex');
 }
